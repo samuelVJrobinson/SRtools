3f874c0ace6f481611352b17e1355a98d903c850
samuelVJrobinson <samuel.vj.robinson@gmail.com>
2021-12-28 20:21:26 -0700

    Updated date function

--- R/dmy2date.R
+++ R/dmy2date.R
@@ -5,20 +5,22 @@
 #' @param month Month column
 #' @param year Year column
 #' @param newName Name for new date column (default = "date")
-#' @param dateFormat Format for date object (default = "%d_%m_%Y", see \code{strptime})
+#' @param dateFormat Date format for each column (default = c("\%d","\%m","\%Y"), see \code{strptime} for more codes)
 #' @return A data frame
 #' @export
+#'
 #' @examples
-#' dat <- data.frame(a=letters[1:2],d=c(10,12),m=c(6,12),y=c(1988,2012))
+#' dat <- data.frame(a=letters[1:2],d=c(10,12),
+#'     m=c(6,12),y=c(1988,2012))
 #' dmy2date(dat,d,m,y,'newDate')
-dmy2date <- function(d,day,month,year,newName='date',dateFormat='%d_%m_%Y'){
+dmy2date <- function(d,day,month,year,newName='date',dateFormat=c('%d','%m','%Y')){
   require(dplyr)
   day <- enquo(day); month <- enquo(month); year <- enquo(year)
   newName <- sym(newName)
 
   d %>%
     unite(!!newName,c({{day}},{{month}},{{year}}),sep='_') %>%
-    mutate(!!newName := as.Date(!!newName,format=dateFormat))
+    mutate(!!newName := as.Date(!!newName,format=paste(dateFormat,collapse='_')))
 }
 
 
