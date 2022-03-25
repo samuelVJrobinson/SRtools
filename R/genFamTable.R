edc37f2f9eba62f931dd119a85859561cb12b605
Samuel Robinson <samuel.vj.robinson@gmail.com>
2022-03-25 14:08:04 -0600

    Added Macropis to genFamTable

--- R/genFamTable.R
+++ R/genFamTable.R
@@ -2,7 +2,7 @@
 #'
 #' A dataset containing two columns, matching common bee genera to families
 #'
-#' @format A data frame with 48 rows and 2 variables:
+#' @format A data frame with 49 rows and 2 variables:
 #' \describe{
 #'   \item{family}{family}
 #'   \item{genus}{genus}
